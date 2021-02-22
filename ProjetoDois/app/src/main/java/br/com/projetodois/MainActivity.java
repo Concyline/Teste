36f1adfe82a3d0fbad0417970839a8c57f736e5c
aureo <concyline@hotmail.com>
2021-02-22 15:27:12 -0300

    fim tela um

--- ProjetoDois/app/src/main/java/br/com/projetodois/MainActivity.java
+++ ProjetoDois/app/src/main/java/br/com/projetodois/MainActivity.java
@@ -2,7 +2,10 @@ package br.com.projetodois;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), TelaUmActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
